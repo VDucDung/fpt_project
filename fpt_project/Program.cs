339f3686f1af84d62d5e90cfc6d229b9d2da543f
VDucDung <dunga3qv2@gmail.com>
2024-07-24 15:19:27 +0700

    Init project

---
+++ fpt_project/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using fpt_project;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        using (var context = new SchoolContext()) 
+        {
+            context.Database.EnsureCreated();
+
+            var grd1 = new Grade() { GradeName = "1st Grade" };
+            var std1 = new Student() { FirstName = "Yash", LastName = "Malhotra", Grade = grd1 };
+
+            context.Students.Add(std1);
+
+            context.SaveChanges();
+
+            foreach (var s in context.Students)
+            {
+                Console.WriteLine($"First Name: {s.FirstName}, Last Name: {s.LastName}");
+            }
+        }
+    }
+}
